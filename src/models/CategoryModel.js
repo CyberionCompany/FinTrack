14ccd28daedf5f365f9ccdbc5485f05f0c7e3ec1
ThaigoDev <thaigodeveloper@gmail.com>
2025-07-30 12:54:50 -0300

    att

--- src/models/CategoryModel.js
+++
@@ -1,6 +0,0 @@
-const CategorySchema = new Schema({
-    name: { type: String, required: true },
-    // A referência ao usuário é opcional para permitir categorias padrão
-    user: { type: Schema.Types.ObjectId, ref: 'Accounts', index: true },
-}, { timestamps: true });
-export const Category = mongoose.model('Category', CategorySchema);
